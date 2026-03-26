8a3507cbc84e3b17c179598018a542557b2302a8
Alvax10 <alvaro695547@gmail.com>
2026-03-26 16:05:56 -0300

    Estados del State Machine

--- scripts/RunningState.gd
+++ scripts/RunningState.gd
@@ -4,6 +4,6 @@ func update(inputs : int, delta : float) -> void:
 	var jumping : bool = (inputs & 1) != 0
 	var slide : bool = (inputs & 2) != 0
 	var throw : bool = (inputs & 4) != 0
-	
+
 	print(jumping)
 	return
